f7e9f1de094532444cde801021fd15752d2c1125
Marco G. Maia <marcogmaia@gmail.com>
2019-05-01 00:43:46 -0300

    remove map files from git

--- assets/maps/Tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Tile" tilewidth="16" tileheight="16" tilecount="32" columns="8">
- <image source="../Objects/Tile.png" width="128" height="64"/>
-</tileset>
